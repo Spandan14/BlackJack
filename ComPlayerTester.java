f4ffeb0da0a22f25aa957b0b0c065636665e6603
ShivaNallapati <60524017+ShivaNallapati@users.noreply.github.com>
2020-02-19 14:25:29 -0600

    Update ComPlayerTester.java

--- ComPlayerTester.java
+++ ComPlayerTester.java
@@ -3,7 +3,7 @@
 /**
  * Write a description of class comPLayerTester here.
  *
- * @author (Shiva Nallapati)
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class ComPlayerTester
